56ed3520fc853afce32860634363f7346c9e2ca2
잼코딩 <147415613+jaemin0619@users.noreply.github.com>
2025-11-10 13:24:09 +0900

    ADD: 요금 DB 추가  (#6)
    * 404 error 해결  (#1) (#2)
    
    * refactor: 파일분리
    
    * refactor: auth/me 추가
    
    * api 403 error 수정
    
    * Refactor: DB 추가 및 임계값 튜닝 추가
    
    * Refactor: DB 추가 및 임계값 튜닝 추가 (#3)
    
    * .gitignore 추가
    
    * frontend 날짜 DB 추가
    
    * dataset 추가

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
+# app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
+from app.core.db import init_db
+from app.models.user_store import seed_users
+
 from app.api.routers.health import router as health_router
 from app.api.routers.auth import router as auth_router
 from app.api.routers.analyze import router as analyze_router
-from app.models.user_store import seed_users
+from app.api.routers.cost import router as cost_router
 
 app = FastAPI(title="MedAnom API")
 
@@ -18,10 +22,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 샘플 유저 시드
+# 초기화
 seed_users()
+init_db()
 
 # 라우터
-app.include_router(health_router, prefix="/api", tags=["default"])
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(analyze_router, prefix="/api", tags=["default"])
+app.include_router(health_router,  prefix="/api",  tags=["default"])
+app.include_router(auth_router,    prefix="/auth", tags=["auth"])
+app.include_router(analyze_router, prefix="/api",  tags=["default"])
+app.include_router(cost_router,    prefix="/api",  tags=["cost"])
